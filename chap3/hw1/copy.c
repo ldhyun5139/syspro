a5ed7945b31f9c46275e7413a3d3aeac6f7fa27e
donghyun <ldhyun5139@naver.com>
2024-09-26 02:58:37 +0000

    hw1

---
+++ chap3/hw1/copy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void copy(char from[], char to []){
+		int i;
+		i = 0;
+		while((to[i]=from[i]) !='\0'){
+			++i;
+			}
+		}
+
+
+		
---
+++ chap3/hw1/copy.h
@@ -0,0 +1,4 @@
+#define MAXLINE 100
+
+void copy(char from[], char to[]);
+
